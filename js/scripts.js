a2d7217f179cf2e690dcdc3dcfc53f9174181c65
Małgorzata Nowak <goskanowak87@gmail.com>
2018-03-09 19:07:37 +0100

    Init project

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var name = prompt('Enter your name');
+// test comment
+alert('Hello, ' + name + ' How are you?');
+/*
+  block comment
+  console.log('The commented will not be called');
+*/
+console.log('Hello, ' + name + ' How are you?');
